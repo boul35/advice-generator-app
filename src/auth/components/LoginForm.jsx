6348f23cb44f2fd74ad281f4dd6f66224c0fcb8d
boul35 <jeanphilippemoffet@gmail.com>
2023-01-26 05:47:15 -0500

    reinstall yup

--- src/auth/components/LoginForm.jsx
+++ src/auth/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component }  from 'react';
 
-
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Input from "../../components/Input";
 import BsForm from "react-bootstrap/Form";
